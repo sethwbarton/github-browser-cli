5310258ec55dcf0e841bcdb8291d3723b239b616
Seth Barton <sethwbarton@gmail.com>
2020-08-19 12:26:31 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+main(process.argv)
+
+async function main (argv) {
+
+}
---
+++ test/find-organization.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const { expect } = require('chai')
+const { describe, it, beforeEach } = require('mocha')
+const sinon = require('sinon')
+
+describe('GitHub Group Lookup', () => {
+  it('Returns information about a GitHub group', () => {
+
+  })
+})
